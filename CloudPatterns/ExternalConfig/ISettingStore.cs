3670049f6d4cfe3f162f0289ef9985b1f075c821
Tristan Dube <tdube@sidecity.com>
2016-06-14 15:13:05 -0400

    Added a MysqlSettingStore. Added dependencies to the Mysql nuget libraries.

--- CloudPatterns/ExternalConfig/ISettingStore.cs
+++ CloudPatterns/ExternalConfig/ISettingStore.cs
@@ -7,16 +7,36 @@ using System.Threading.Tasks;
 namespace CloudPatterns.ExternalConfig
 {
     /// <summary>
-    /// 
+    /// This class is used to access a configuration file independant on the storage
+    /// location of the config.
     /// </summary>
     public interface ISettingStore
     {
+        /// <summary>
+        /// Get the version string of the config file. This number changes after each change in the config.
+        /// </summary>
         string Version { get; }
 
+        /// <summary>
+        /// Get all the configuration values.
+        /// </summary>
+        /// <returns>Dictionary of key/value config value pairs.</returns>
         Dictionary<string, string> FindAll();
 
+        /// <summary>
+        /// Get a specific value buy key name.
+        /// </summary>
+        /// <param name="key">Key name</param>
+        /// <returns>Value found</returns>
         string Get(string key);
 
+        /// <summary>
+        /// Update a config value.
+        /// </summary>
+        /// <param name="key">Key of the config value to change</param>
+        /// <param name="value">New value</param>
+        /// <remarks>This function is so far not implemented, updating the config file
+        /// from clients is not trivial.</remarks>
         void Update(string key, string value);
     }
 }
---
+++ CloudPatterns/ExternalConfig/MySQLSettingStore.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace CloudPatterns.ExternalConfig
+{
+    public class MySQLSettingStore : ISettingStore
+    {
+        private MySqlConnection _db;
+        private string _configTableName;
+        private string _configKeyColumnName;
+        private string _configValueColumnName;
+
+        public MySQLSettingStore(MySqlConnection connection, string configTableName, string configKeyColumnName, string configValueColumnName)
+        {
+            _db = connection;
+            _configTableName = configTableName;
+            _configKeyColumnName = configKeyColumnName;
+            _configValueColumnName = configValueColumnName;
+        }
+
+        public string Version { get; private set; }
+
+        public Dictionary<string, string> FindAll()
+        {
+            Dictionary<string, string> result = new Dictionary<string, string>();
+
+            MySqlCommand cmd = _db.CreateCommand();
+            cmd.CommandText = String.Format("SELECT {0}, {1} FROM {2}", _configKeyColumnName, _configValueColumnName, _configTableName);
+            cmd.CommandType = System.Data.CommandType.Text;
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.HasRows)
+            {
+                string keyName = rdr.GetString(_configKeyColumnName);
+                string value = rdr.GetString(_configValueColumnName);
+
+                result.Add(keyName, value);
+
+                rdr.Read();
+            }
+
+            rdr.Close();
+
+            return result;
+        }
+
+        public string Get(string key)
+        {
+            MySqlCommand cmd = _db.CreateCommand();
+            cmd.CommandText = String.Format("SELECT {0} FROM {1} WHERE {2} = {3}", _configValueColumnName, _configTableName, _configKeyColumnName, key);
+            cmd.CommandType = System.Data.CommandType.Text;
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            string result = "";
+
+            if (rdr.HasRows)
+            {
+                result = rdr.GetString(_configKeyColumnName);
+            }
+            else
+            {
+                result = null;
+            }
+
+            rdr.Close();
+
+            return result; ;
+        }
+
+        public void Update(string key, string value)
+        {
+        }
+    }
+}
