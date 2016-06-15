e9fb751ca4d1778ff1dd066a4cf416136e0b614c
Tristan Dube <tdube@sidecity.com>
2016-06-15 18:22:32 -0400

    Started adding custom appenders.

---
+++ CloudPatterns/Logging/AzureFileAppender.cs
@@ -0,0 +1,30 @@
+﻿using log4net.Appender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Core;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace CloudPatterns.Logging
+{
+    public class AzureFileAppender : AppenderSkeleton
+    {
+        public string ConnectionstringSettingname { get; set; }
+
+        public string ContainerName { get; set; }
+
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            CloudStorageAccount account = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting(ConnectionstringSettingname));
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer Container = client.GetContainerReference(ContainerName);
+            Container.CreateIfNotExists();
+            Container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+        }
+    }
+}
---
+++ CloudPatterns/Logging/DebugAppender.cs
@@ -0,0 +1,28 @@
+﻿using log4net.Appender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net.Core;
+
+namespace CloudPatterns.Logging
+{
+    public class DebugAppender : AppenderSkeleton
+    {
+        protected override void Append(LoggingEvent loggingEvent)
+        {
+            string renderedMessage = RenderLoggingEvent(loggingEvent);
+
+            // Add debug code here as needed.
+        }
+
+        protected override bool RequiresLayout
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+}
