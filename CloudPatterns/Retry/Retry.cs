a0db3bd8d38a1f07b8105a48a433df8af61a1401
Tristan Dube <tdube@sidecity.com>
2016-06-22 10:42:38 -0400

    Added documentation and cleaned variable names in the Retry class.

--- CloudPatterns/Retry/Retry.cs
+++ CloudPatterns/Retry/Retry.cs
@@ -7,34 +7,81 @@ using System.Threading.Tasks;
 
 namespace CloudPatterns.Retry
 {
-    public class Retry
+    /// <summary>
+    /// Class that handles a single operation that can be re-tried every X miliseconds until a limit timeout has been hit.
+    /// 
+    /// This class is used to be triggered only once, if it should be launched a second time a new object should be recreated.
+    /// </summary>
+    /// <typeparam name="T">Generic type</typeparam>
+    public class Retry<T>
     {
-        public async Task<T> RetryFunc<T>(Func<T> func, int intervalTime, int totalTimeout)
+        private Func<T> FunctionToCall;
+        private int IntervalTime;
+        private int TotalTimeout;
+
+        private CancellationTokenSource MyCancellingSource;
+        private CancellationToken CurrentOperationCancelToken;
+
+        /// <summary>
+        /// Constructor to build the object in a runnable state.
+        /// </summary>
+        /// <param name="func">Function to call and retry</param>
+        /// <param name="intervalTime">Span of time during which an operation will be awaited for.</param>
+        /// <param name="totalTimeout">Duration after which the whole Retry operation will be stopped and cancelled.</param>
+        public Retry(Func<T> func, int intervalTime, int totalTimeout)
         {
-            DateTime timeoutLimit = DateTime.Now.Add(TimeSpan.FromMilliseconds(totalTimeout));
+            FunctionToCall = func;
+            IntervalTime = intervalTime;
+            TotalTimeout = totalTimeout;
 
-            while(true)
+            MyCancellingSource = new CancellationTokenSource();
+            CurrentOperationCancelToken = MyCancellingSource.Token;
+        }
+
+        /// <summary>
+        /// Start the operation.
+        /// </summary>
+        /// <remarks>This operation should be started asynchronously.</remarks>
+        /// <returns>Returns a task that can be awaited for results.</returns>
+        public async Task<T> Start()
+        {
+            DateTime timeoutLimit = DateTime.Now.Add(TimeSpan.FromMilliseconds(TotalTimeout));
+
+            while(CurrentOperationCancelToken.IsCancellationRequested == false)
             {
-                CancellationTokenSource source = new CancellationTokenSource();
-                CancellationToken x = source.Token;
+                CancellationTokenSource currentIntervalSource = new CancellationTokenSource();
+                CancellationToken currentIntervalToken = currentIntervalSource.Token;
 
-                Task<T> wanted = new Task<T>(func, x);
+                Task<T> wanted = new Task<T>(FunctionToCall, currentIntervalToken);
 
-                source.CancelAfter(TimeSpan.FromMilliseconds(intervalTime));
-                var result = await wanted;
+                // Start a cancel until the next interval starts
+                currentIntervalSource.CancelAfter(TimeSpan.FromMilliseconds(IntervalTime));
+
+                var result = await wanted; // Start the operation and block until it's done.
 
                 if (wanted.Status == TaskStatus.RanToCompletion)
                 {
                     return result;
                 }
 
-                if (DateTime.Now > timeoutLimit)
+                // Not sure which to use, check timeout expiration or if it was cancelled.
+                if (DateTime.Now > timeoutLimit || currentIntervalToken.IsCancellationRequested)
                 {
                     return default(T);
                 }
 
-                source.Dispose();
+                currentIntervalSource.Dispose();
             }
+
+            return default(T); // Cancelled
+        }
+
+        /// <summary>
+        /// Cancel the current running operation.
+        /// </summary>
+        public void Cancel()
+        {
+            MyCancellingSource.Cancel();
         }
     }
 }
