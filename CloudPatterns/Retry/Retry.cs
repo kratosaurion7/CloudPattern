fa9c97c76c9c5e285516897795d37e704e093e0e
Tristan Dube <tristan.dube@gmail.com>
2016-06-21 23:44:45 -0400

    Added tentative implementation of the Retry pattern.

---
+++ CloudPatterns/Retry/Retry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CloudPatterns.Retry
+{
+    public class Retry
+    {
+        public async Task<T> RetryFunc<T>(Func<T> func, int intervalTime, int totalTimeout)
+        {
+            DateTime timeoutLimit = DateTime.Now.Add(TimeSpan.FromMilliseconds(totalTimeout));
+
+            while(true)
+            {
+                CancellationTokenSource source = new CancellationTokenSource();
+                CancellationToken x = source.Token;
+
+                Task<T> wanted = new Task<T>(func, x);
+
+                source.CancelAfter(TimeSpan.FromMilliseconds(intervalTime));
+                var result = await wanted;
+
+                if (wanted.Status == TaskStatus.RanToCompletion)
+                {
+                    return result;
+                }
+
+                if (DateTime.Now > timeoutLimit)
+                {
+                    return default(T);
+                }
+
+                source.Dispose();
+            }
+        }
+    }
+}
